bf4c48e7b94d1de11a497c086a66f2392775733c
tawatat@rambler.ru <283163aA>
2022-06-06 23:25:47 +0300

    0.1

---
+++ AddAnEvent.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+
+public class AddAnEvent {
+    private String name;
+    private LocalDate date;
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+
+
+
+}
--- BirthDay.java
+++ BirthDay.java
@@ -1,5 +1,8 @@
 public class BirthDay {
     public static void main(String[] args) {
-        System.out.println("jheg");
+        AddAnEvent g = new AddAnEvent();
+
+
+
     }
 }
